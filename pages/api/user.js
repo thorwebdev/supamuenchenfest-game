47e678d2471a564affd7ed2b0b95dc809656aaf7
thorwebdev <thor@supabase.io>
2022-03-18 10:20:06 +0100

    what is your name

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,16 +1,25 @@
-import * as uuid from 'uuid'
-import rateLimit from '../../utils/rate-limit'
+import * as uuid from "uuid";
+import rateLimit from "../../utils/rate-limit";
 
 const limiter = rateLimit({
   interval: 60 * 1000, // 60 seconds
   uniqueTokenPerInterval: 500, // Max 500 users per second
-})
+});
 
 export default async function handler(req, res) {
   try {
-    await limiter.check(res, 10, 'CACHE_TOKEN') // 10 requests per minute
-    res.status(200).json({ id: uuid.v4() })
+    await limiter.check(res, 10000, "CACHE_TOKEN"); // 10000 requests per minute
+
+    const { q } = req.query;
+
+    if (q.toLowerCase().contains("what is your name")) {
+      console.log("q:", req.query.q);
+      console.log("Method:", req.method);
+      console.log("Body:", req.body);
+    }
+
+    res.status(200).end("Kopfsteinhammer");
   } catch {
-    res.status(429).json({ error: 'Rate limit exceeded' })
+    res.status(429).json({ error: "Rate limit exceeded" });
   }
 }
