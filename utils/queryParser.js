6f54ded14f8e51fffaea6181e6b7fd7fb1d53e51
Copple <10214025+kiwicopple@users.noreply.github.com>
2022-03-18 11:22:52 +0100

    Adds facts

--- utils/queryParser.js
+++ utils/queryParser.js
@@ -1,11 +1,15 @@
 export const facts = (req) => {
   const q = req.query?.q;
-  const fact = q.split(": ")[1];
+  const fact = q.split(": ")[1].toLowerCase();
 
   switch (fact) {
     case "what color is a banana":
     case "what colour is a banana":
       return "yellow";
+    case "what is your location":
+      return "munich";
+    case "roses are red violets are blue":
+      return "sugar is sweet, and so are you.";
     default:
       console.log("Unknown fact", q);
       return "";
